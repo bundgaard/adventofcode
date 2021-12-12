5802e044889336007d2fd2f91ae8ef14eb46f12a
David Poulsen <dapoman@gmail.com>
2021-12-12 13:13:07 +0100

    Day11 Part 01

--- 2021/day11/main.go
+++ 2021/day11/main.go
@@ -56,12 +56,32 @@ func step(consortium [][]int) {
 		for x := 0; x < len(consortium[0]); x++ {
 			if consortium[y][x]-1 == 0 {
 				flash++
-				// consortium[y-1][x]--
-				// nw  n ne -1,-1  0,-1 1,-1
-				// w c e    -1, 0, 0, 0 1, 0
-				// sw s se   1, 1  0, 1 1, 1
+				// 5 1 5
+				// 5 5 5
+
+				for i := -1; i < 2; i++ {
+					for j := -1; j < 2; j++ {
+
+						log.Printf("%d,%d", i, j)
+
+					}
+				}
+				/*consortium[y-1][x-1]-- // nw
+				consortium[y-1][x+0]-- // n
+				consortium[y-1][x+1]-- // ne
+
+				consortium[y][x-1]-- // w
+				consortium[y][x+0]-- // c
+				consortium[y][x+1]-- // e
+
+				consortium[y+1][x-1]-- // sw
+				consortium[y+1][x+0]-- // s
+				consortium[y+1][x+1]-- // se
+				*/
+			} else {
+				consortium[y][x]--
 			}
-			consortium[y][x]--
+
 		}
 	}
 
